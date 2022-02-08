5c8b7231adfa78608204a887523148b09ff79fb0
d-smith <doug.smith.mail@gmail.com>
2022-02-07 19:14:15 -0800

    word weight update

--- readme.md
+++ readme.md
@@ -4,9 +4,14 @@ Messing around with wordle...
 
 * Initial start - use letter frequency to score words
 * Increase scrore for vowels, penalty for duplicate letters (more info gain?)
-* Next - factor in word frequency, maybe... not sure if puzzle word choice is random
 
 Ranked word source - https://en.lexipedia.org/
 
 word, length, freq, number of articles
+
+Next
+
+positionIs
+positionIsNot
+weights based on game word list not general word list
 
--- src/main/java/org/ds/flink/wordle/WordMatch.java
+++ src/main/java/org/ds/flink/wordle/WordMatch.java
@@ -16,7 +16,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordMatch {
-    private static Map<Character, Integer> charWeights=
+    private static Map<Character, Integer> charWeightsOrig=
             Map.ofEntries(
                 Map.entry('e',6892),
                 Map.entry('a',6758),
@@ -45,7 +45,35 @@ public class WordMatch {
                 Map.entry('j',286),
                 Map.entry('q',128)
             );
-
+    private static Map<Character, Integer> charWeights=
+            Map.ofEntries(
+                    Map.entry('s',6665),
+                    Map.entry('e',6662),
+                    Map.entry('a',5990),
+                    Map.entry('o',4438),
+                    Map.entry('r',4158),
+                    Map.entry('i',3759),
+                    Map.entry('l',3371),
+                    Map.entry('t',3295),
+                    Map.entry('n',2952),
+                    Map.entry('u',2511),
+                    Map.entry('d',2453),
+                    Map.entry('y',2074),
+                    Map.entry('c',2028),
+                    Map.entry('p',2019),
+                    Map.entry('m',1976),
+                    Map.entry('h',1760),
+                    Map.entry('g',1644),
+                    Map.entry('b',1627),
+                    Map.entry('k',1505),
+                    Map.entry('f',1115),
+                    Map.entry('w',1039),
+                    Map.entry('v',694),
+                    Map.entry('z',434),
+                    Map.entry('j',291),
+                    Map.entry('x',288),
+                    Map.entry('q',112)
+            );
 
     private static boolean doesNotHave(String s, char... chars) {
         for(char c: chars) {
